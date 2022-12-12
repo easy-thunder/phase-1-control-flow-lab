23a16da40925c0ee73240efe1e821949f01fbc33
Jacob David Diehl <jakediehl17@gmail.com>
2022-12-12 08:52:26 -0700

    commit tree 85d9fc96490dbc563752cd4700ae084c63674e9a

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
-}
+  if(feet>0){
+  if(feet<=400){
+    return "This one is on me!"
+  }
+  else if(feet>400 && feet <= 2000){
+    return "That will be twenty bucks."
+  }
+  else if (feet>2000 && feet <= 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else {return"No can do."}
+}}
+function ternaryCheckCity(city){
+  
+  // Write your code here!
+ let message;
+ message = city=== "NYC" ? 'Ok, sounds good.' : 'No go.'
+ return message;
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye.";
+      
+ }
 }
